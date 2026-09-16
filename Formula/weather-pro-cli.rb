2d5e202df3ea2aaf464832ed94628a6d9ad791a4
Alessandro Bresciani <cyberroute@proton.me>
2026-09-16 16:45:06 +0200

    Update homepage URL for WeatherProCli

--- Formula/weather-pro-cli.rb
+++ Formula/weather-pro-cli.rb
@@ -1,6 +1,6 @@
 class WeatherProCli < Formula
   desc "Simple CLI that reports the weather for a city"
-  homepage "https://github.com/USER/weather-pro-cli"
+  homepage "https://github.com/CyberRoute/weather-pro-cli"
   url "https://github.com/CyberRoute/weather-pro-cli/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0e2f6583ffcbd7a2f7e1dd6496f0fac59d8937d47fa1116a12bf091f2949db57"
   license "MIT"
