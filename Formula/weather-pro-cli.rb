aed9f8247776c20145acb9a01560c2bf1c7c5daf
Alessandro Bresciani <cyberroute@proton.me>
2026-09-21 11:26:19 +0200

    Update URL to version 1.1.1 in weather-pro-cli.rb

--- Formula/weather-pro-cli.rb
+++ Formula/weather-pro-cli.rb
@@ -1,7 +1,7 @@
 class WeatherProCli < Formula
   desc "Simple CLI that reports the weather for a city"
   homepage "https://github.com/CyberRoute/weather-pro-cli"
-  url "https://github.com/CyberRoute/weather-pro-cli/archive/refs/tags/v1.1.0.tar.gz"
+  url "https://github.com/CyberRoute/weather-pro-cli/archive/refs/tags/v1.1.1.tar.gz"
   sha256 "5f220d57d7d835dced96dd7ed883ece1bab50b041a266c7793eccf7dac691220"
   license "MIT"
 
